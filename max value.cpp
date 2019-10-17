3e1924acbb02adf5831df1f88d4e43b607ebd7d7
Kateryna Trofymenko <ekaterinatrofa@gmail.com>
2019-10-17 13:29:19 +0300

    a

--- max value.cpp
+++ max value.cpp
@@ -6,11 +6,11 @@ using namespace std;
 int main()
 {
      int a, b, c;
-    cout << "Podaj pierwsze liczbe A" << endl;
+    cout << "Podaj pierwsze liczbe a" << endl;
     cin >> a;
-    cout << "Podaj drugie liczbe B" << endl;
+    cout << "Podaj drugie liczbe b" << endl;
     cin >> b;
-    cout << "Podaj trzecie liczbe C" << endl;
+    cout << "Podaj trzecie liczbe c" << endl;
     cin >> c;
     if ( a > c && a > b ) cout << "Wieksza liczba " << a << endl;
     if ( b > a && b > c ) cout << "Wieksza liczba " << b << endl;
